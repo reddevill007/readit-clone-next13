fe0af570fdbdb4a0c397645b8d48f3f73da3f3a5
reddevill007 <shivampandey2900@gmail.com>
2023-07-09 07:56:44 +0530

    next auth config

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,13 +1,42 @@
-import React, { FC } from "react";
+"use client";
+
+import React, { FC, useState } from "react";
 import { Button } from "./ui/Button";
 import { cn } from "@/lib/utils";
+import { signIn } from "next-auth/react";
+import { Icons } from "./Icons";
+import { useToast } from "@/hooks/use-toast";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+    try {
+      await signIn("google");
+    } catch (error) {
+      toast({
+        title: "There was a problem",
+        description: "There was an error logging in with Google",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className={cn("flex justify-center", className)} {...props}>
-      <Button className="w-full" size="sm">
+      <Button
+        className="w-full"
+        size="sm"
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+      >
+        {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
         Google
       </Button>
     </div>
